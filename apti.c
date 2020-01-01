c886c6e79205b16866a988bf188de82331109454
Andrew Akhash <36655762+andrewak11@users.noreply.github.com>
2020-01-01 18:56:24 +0530

    new file
    8th commit  i guess...

---
+++ apti.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// int main()
+// {
+//     static int x=3;
+//     if(x--)
+//     {
+//         main();
+//         printf("%d",x);
+//     }
+// }
+
+int main()
+{
+    char a= 'a',b='b';
+    printf("%d",abs(a-b));
+}
